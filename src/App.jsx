9b70d562fe4f0af98df7f5e86db30fe7cda92e30
Caleb Allen <caleba98@hotmail.co.uk>
2021-06-10 23:55:02 +0100

    chore: fix cname and tidy a bit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          there's nothing here at the moment.
+        </p>
+        <p>☁</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
